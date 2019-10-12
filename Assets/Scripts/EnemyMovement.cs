1ad0c6d1e841e0bd326e53ba49e798919b8af8bb
AlbertoLeitao <46546389+AlbertoLeitao@users.noreply.github.com>
2019-10-12 12:36:45 -0400

    enemy

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,27 +6,90 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
     Rigidbody2D myRigiBody;
+
+    public Transform target;
+    private float distancePlayer;
+    private Animator anim;
+    private Rigidbody2D rigiBody2D;
+    private float maxSpeed = 5f;
+    private Vector2 runRight;
+    private Vector2 runLeft;
+    private Vector2 stop;
+    private bool facingRight = true;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigiBody2D = GetComponent<Rigidbody2D>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         myRigiBody = GetComponent<Rigidbody2D>();
+
+        runRight = new Vector2(maxSpeed, 0);
+        runLeft = new Vector2(-maxSpeed, 0);
+        stop = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (IsFacingRight()) 
+
+     
+
+        // Al ver al jugador
+        distancePlayer = target.position.x - transform.position.x;
+        if(distancePlayer > 0 && !facingRight)
         {
-            myRigiBody.velocity = new Vector2(moveSpeed, 0);
+            Flip();
+        }
+        else if(distancePlayer < 0 && facingRight)
+        {
+         Flip();
+        }
+        /*
+        if(distancePlayer < -1f)
+        {
+            rigiBody2D.velocity = runLeft;
+        }
+        else if(distancePlayer > 1f)
+        {
+            rigiBody2D.velocity = runRight;
         }
         else
         {
-            myRigiBody.velocity = new Vector2(-moveSpeed, 0);
+            myRigiBody.velocity = stop;
+        } */
+
+        // Camine sin ver al jugador
+        if (IsFacingRight())
+        {
+            rigiBody2D.velocity = new Vector2(moveSpeed, 0);
         }
-        
+        else
+        {
+            rigiBody2D.velocity = new Vector2(-moveSpeed, 0);
+        }
+
+
+
     }
 
-   bool IsFacingRight()
+    private void FixedUpdate()
+    {
+        anim.SetFloat("Distance", Mathf.Abs(distancePlayer));
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    bool IsFacingRight()
     {
         return transform.localScale.x > 0;
     }
