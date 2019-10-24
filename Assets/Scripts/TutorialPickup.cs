50e0d298b7902d588e68f86c60caf08e704ab81a
Nicolas Silveira <46078503+NikoSilveira@users.noreply.github.com>
2019-10-24 18:57:02 -0400

    Colision-dialogo habilitado

---
+++ Assets/Scripts/TutorialPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPickup : MonoBehaviour
+{
+
+    PlayerPhone PlayerP;
+    //DialogueTrigger dTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerP = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerPhone>();
+        //dTrigger = GameObject.FindGameObjectWithTag("DialogueManager").GetComponent<DialogueTrigger>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Cero0");
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Uno1");
+            DialogueTrigger dTrigger = GameObject.FindGameObjectWithTag("Book").GetComponent<DialogueTrigger>();
+            Debug.Log("Dos2");
+            dTrigger.TriggerDialogue();
+            Debug.Log("Tres3");
+            Destroy(gameObject);
+        }
+    }
+
+}
