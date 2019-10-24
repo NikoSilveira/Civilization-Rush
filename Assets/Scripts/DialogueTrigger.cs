da45237b63b72a340216b6a04117224efdaf7731
Nicolas Silveira <46078503+NikoSilveira@users.noreply.github.com>
2019-10-23 23:01:39 -0400

    manejador de dialogos

---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        //Aqui se puede implementar un singleton
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+}
