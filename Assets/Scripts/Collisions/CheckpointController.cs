e89325dd345647133f3d2a2f019f34c19c9475bf
Nicolas Silveira <nsvh22@gmail.com>
2019-10-29 18:35:51 -0400

    doc interna checkpointcontroller

--- Assets/Scripts/Collisions/CheckpointController.cs
+++ Assets/Scripts/Collisions/CheckpointController.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/*
+    -Este script es utilizado por PlayerPhone para 
+    obtener la variable de control del checkpoint
+    -PlayerPhone responde guardando un vector3 de la posición
+    del jugador durante la colisión con el checkpoint
+*/
+
 public class CheckpointController : MonoBehaviour
 {
 
+    //Control dinámico del sprite
     public Sprite redFlag;
     public Sprite blueFlag;
     private SpriteRenderer checkpointSpriteRenderer;
+
+    //Bool de control de activación del checkpoint
     public bool checkpointReached;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +35,13 @@ public class CheckpointController : MonoBehaviour
         
     }
 
+
+    //Colisión Player - Checkpoint
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && checkpointSpriteRenderer.sprite == redFlag)
         {
+            //Activar checkpoint
             checkpointSpriteRenderer.sprite = blueFlag;
             checkpointReached = true;
 
